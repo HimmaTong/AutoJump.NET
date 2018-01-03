a9a0f6c0b1eb75d14a75779ce8f26d9b0757c2fc
experdot <experdot@foxmail.com>
2018-01-03 13:30:57 +0800

    Convert AutoJump.Platform.IOS library to csharp
    there is no specific implementation

---
+++ AutoJump.Platform.IOS/Device/IOSDevice.cs
@@ -0,0 +1,31 @@
+﻿using AutoJump.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoJump.Platform.IOS
+{
+    /// <summary>
+    /// 表示一个实现了<see cref="IDevice"/>的IOS设备
+    /// </summary>
+    public class IOSDevice : IDevice
+    {
+        public bool Availiable => throw new NotImplementedException();
+
+        public Size Size => throw new NotImplementedException();
+
+        public Image GetScreenImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Press(Vector2 position, int millionseconds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
